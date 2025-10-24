b75df1d163a2830b04ca95c7de270135760d3901
Joseph Raymund Ogapong <jogapong@gmail.com>
2025-10-24 21:13:05 +0800

    integrated firebase in the app

---
+++ firebase/firebaseConfig.js
@@ -0,0 +1,33 @@
+import { initializeApp } from 'firebase/app';
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
+// Optionally import the services that you want to use
+// import {...} from 'firebase/auth';
+// import {...} from 'firebase/database';
+// import {...} from 'firebase/firestore';
+// import {...} from 'firebase/functions';
+// import {...} from 'firebase/storage';
+
+// Initialize Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyBx4N57heX9B5ReQp-cOvo9ntUfmppl5FU",
+  authDomain: "play-pulse-68ff9.firebaseapp.com",
+  databaseURL: "https://play-pulse-68ff9-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "play-pulse-68ff9",
+  storageBucket: "play-pulse-68ff9.firebasestorage.app",
+  messagingSenderId: "526875205839",
+  appId: "1:526875205839:web:6bbdcb6cca8e36989657ba",
+  measurementId: "G-2ZV9LDEBT1"
+};
+
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const db = getFirestore(app);
+
+async function getCities(db) {
+  const citiesCol = collection(db, 'cities');
+  const citySnapshot = await getDocs(citiesCol);
+  const cityList = citySnapshot.docs.map(doc => doc.data());
+  return cityList;
+}
